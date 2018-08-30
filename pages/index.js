887722f994ca9e196a015af6d866cef1132a2612
Zach Garwood <zachgarwood@gmail.com>
2018-08-30 18:55:38 -0500

    Project initialization

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import styled from 'react-emotion';
+
+const Header = styled('h1')`
+  color: blue;
+`
+
+const Index = () => (
+  <Header>Hello, Difference Engine!</Header>
+)
+
+export default Index
